e63b4ede3611a7497c47d632763dc4ede5e66ad3
James Kiesel <james.kiesel@gmail.com>
2015-12-04 12:28:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Teleport - Take you to a top search result instantly
+
+Teleport exposes an action which allows users to input a search term, and takes them immediately to the most relevant search result.
+
+### Installation
+- Edit your web template and add the project clone url. (https://meta.discourse.org/t/install-a-plugin/19157)
+- Rebuild your web container so that the plugin installs.
+
+You're done!
+
+### Usage
+
+In order to teleport, simple visit the route `/teleport/search?q=<your_query_here>`.
+Teleport will automatically pull up the most relevant topic search result and redirect you there. :zap:
+
+### Contributing
+
+Pull requests welcome! To contribute:
+- Fork it ( https://github.com/<your-github-username>/teleport/fork )
+- Create your feature branch (`git checkout -b your-new-feature`)
+- Commit your changes (`git commit -am 'Add some feature`)
+- Push to the branch (`git push origin your-new-feature`)
+- Create a new Pull Request
---
+++ plugin.rb
@@ -0,0 +1,50 @@
+# name: teleport
+# about: Instant search results for Discourse
+# version: 0.0.1
+# authors: James Kiesel (gdpelican)
+# url: https://github.com/gdpelican/teleport
+
+TELEPORT_PLUGIN_NAME ||= "teleport".freeze
+
+enabled_site_setting :teleport_enabled
+
+after_initialize do
+  module ::Teleport
+    class Engine < ::Rails::Engine
+      engine_name TELEPORT_PLUGIN_NAME
+      isolate_namespace Teleport
+    end
+  end
+
+  ::Teleport::Engine.routes.draw do
+    get "/search" => "search#search"
+  end
+
+  Discourse::Application.routes.append do
+    mount ::Teleport::Engine, at: "/teleport"
+  end
+
+  class ::Teleport::SearchController < ApplicationController
+    layout 'no_ember'
+    skip_before_filter :check_xhr
+
+    def search
+      raise Discourse::NotFound unless search_results && search_results.posts.any?
+      redirect_to search_results.posts.first.topic.url
+    end
+
+    private
+
+    def search_results
+      @search_results ||= Search.new(params[:q], search_args).execute
+    end
+
+    def search_args
+      {
+        type_filter: 'topic',
+        guardian: guardian,
+        include_blurbs: false
+      }
+    end
+  end
+end
